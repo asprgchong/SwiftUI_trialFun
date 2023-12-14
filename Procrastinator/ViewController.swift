4f85d2807b19bd12db45b78c739c3daf75b23696
Geralyn <geralynchong26@gmail.com>
2023-12-14 12:48:52 +0800

    CodePath Prework

--- Procrastinator/ViewController.swift
+++ Procrastinator/ViewController.swift
@@ -13,7 +13,28 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func ChangeBackgroundColor(_ sender: UIButton) {
+        let randomColor = changeColor()
+        let text = changeColor()
+        view.backgroundColor = randomColor
+        NameTitle.textColor = text
+    }
+    
+    @IBOutlet weak var NameTitle: UILabel!
+   
+    @IBAction func ResetBackgroundColor(_ sender: UIButton) {
+        view.backgroundColor = UIColor.white
+        NameTitle.textColor = UIColor.black
+    }
+    
+    func changeColor() -> UIColor{
+        let red = CGFloat.random(in: 0...1)
+        let green = CGFloat.random(in: 0...1)
+        let blue = CGFloat.random(in: 0...1)
 
-
+        return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+    }
 }
+
 
