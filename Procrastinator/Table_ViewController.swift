9b799da9ffd986d2e813795c0168d6069757113b
Geralyn <geralynchong26@gmail.com>
2023-12-15 09:46:56 +0800

    Implementing more functionality

---
+++ Procrastinator/Table_ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  Table_ViewController.swift
+//  Procrastinator
+//
+//  Created by Geralyn Chong on 14/12/23.
+//
+
+import UIKit
+
+class Table_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
